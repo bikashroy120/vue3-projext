81c0f724055698ee271a5528c7875aba30bf2b92
thinkwithabs <twithabs@gmail.com>
2023-01-07 13:22:30 +0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="p-8 flex justify-center flex-col gap-3">
+        <input type="text" class="w-full " placeholder="Search Meales...">
+        <div class="flex items-center justify-center gap-2">
+            <router-link :to="{name:'byLetter', params:{letter}}" v-for="letter of letters" :key="{letter}">
+                    {{ letter }}
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {computed} from "vue";
+import store from "../store";
+
+    const bikash = computed(()=>store.state.bikash)
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+
+</script>
---
+++ src/views/MealList.vue
@@ -0,0 +1,10 @@
+<template>
+
+    <div>hello, this is our Meal List Page</div>
+
+</template>
+
+
+<script setup>
+
+</script>
